c949caba55f7707a604b7321f6163b23b1a0cb3d
Alkawe <alixkwebb@gmail.com>
2020-11-04 20:11:08 -0500

    added router paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path={["/", "/about"]}>
+            <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/portfolio">
+            <Portfolio />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
