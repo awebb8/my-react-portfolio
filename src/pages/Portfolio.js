7eca52642a55defda6e29e6a687414687527447a
Alkawe <alixkwebb@gmail.com>
2020-11-04 19:48:20 -0500

    linked components

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PortfolioCard from '../components/PortfolioCard';
+
+function Portfolio() {
+    return (
+        <div>
+            <PortfolioCard />
+        </div>
+    );
+};
+
+export default Portfolio;
