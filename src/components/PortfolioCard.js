361f8a08ab94cfdcbd7a3dcf786b4bbd02f789ac
Alkawe <alixkwebb@gmail.com>
2020-12-08 22:22:10 -0500

    modified styling and added allues links

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -7,17 +7,58 @@ import noteTaker from "../assets/images/note-taker.png";
 import whatsForDinner from "../assets/images/project-1-whats-for-dinner.png";
 import readmeGenerator from "../assets/images/readme-generator.png";
 import weatherDashboard from "../assets/images/weather-dashboard.png";
+import AllHues from "../assets/images/allhues-homepage.png";
 
 function PortfolioCard() {
     return (
-        <section className="container-fluid mx-4 my-5">
-            <div className="row">
-            <div className="col-sm-10">
+      <section className="container-fluid" style={{margin:"3%"}}>
+      <div className="row">
+          <div style={{width:"94%"}}>
                   <div className="card main-card">
                     <div className="card-header">
                       <h1>Portfolio</h1>
                     </div>
                     <div className="card-body">
+
+                    <div className="card project-card" style={{width:"26rem"}}>
+                      <img className="crop project-image" src={AllHues} alt="AllHues"  style={{width:"350px", height:"350px"}}/>
+                      <div className="card-body" style={{textAlign:"center"}}>
+                        <h5 className="card-title">AllHues</h5>
+                        <a className="project-links" href ="https://github.com/awebb8/allhues" target="_blank">View Github</a>
+                        <b> • </b>
+                        <a className="project-links" href="https://allhues.herokuapp.com/" target="_blank">View Application</a>
+                      </div>
+                    </div>
+
+                    <div className="card project-card" style={{width:"26rem"}}>
+                      <img className="crop project-image" src={whatsForDinner} alt="project-1" style={{width:"350px", height:"350px"}}/>
+                      <div className="card-body" style={{textAlign:"center"}}>
+                        <h5 className="card-title">What's for Dinner</h5>
+                        <a className="project-links" href ="https://github.com/awebb8/project-1" target="_blank">View Github</a>
+                        <b> • </b>
+                        <a className="project-links" href="https://awebb8.github.io/project-1/" target="_blank">View Application</a>
+                      </div>
+                    </div>
+
+                    <div className="card project-card" style={{width:"26rem"}}>
+                      <img className="crop project-image" src={eatDaBurger} alt="eat-da-burger" style={{width:"350px", height:"350px"}}/>
+                      <div className="card-body" style={{textAlign:"center"}}>
+                        <h5 className="card-title">Eat Da Burger</h5>
+                        <a className="project-links" href="https://github.com/awebb8/eat-da-burger" target="_blank">View Github</a>
+                        <b> • </b>
+                        <a className="project-links" href="https://eat-da-burger-alix.herokuapp.com/" target="_blank">View Application</a>
+                      </div>
+                    </div>
+
+                    <div className="card project-card" style={{width:"26rem"}}>
+                      <img className="crop project-image" src={noteTaker} alt="note-taker" style={{width:"350px", height:"350px"}}/>
+                      <div className="card-body" style={{textAlign:"center"}}>
+                        <h5 className="card-title">Note Taker</h5>
+                        <a className="project-links" href="https://github.com/awebb8/note-taker" target="_blank">View Github</a>
+                        <b> • </b>
+                        <a className="project-links" href="https://obscure-island-16380.herokuapp.com/" target="_blank">View Application</a>
+                      </div>
+                    </div>
 
                     <div className="card project-card" style={{width:"26rem"}}>
                       <img className="crop project-image" src={javascriptQuiz} alt="javascript-quiz"  style={{width:"350px", height:"350px"}}/>
@@ -40,16 +81,6 @@ function PortfolioCard() {
                     </div>
 
                     <div className="card project-card" style={{width:"26rem"}}>
-                      <img className="crop project-image" src={whatsForDinner} alt="project-1" style={{width:"350px", height:"350px"}}/>
-                      <div className="card-body" style={{textAlign:"center"}}>
-                        <h5 className="card-title">What's for Dinner</h5>
-                        <a className="project-links" href ="https://github.com/awebb8/project-1" target="_blank">View Github</a>
-                        <b> • </b>
-                        <a className="project-links" href="https://awebb8.github.io/project-1/" target="_blank">View Application</a>
-                      </div>
-                    </div>
-
-                    <div className="card project-card" style={{width:"26rem"}}>
                       <img className="crop project-image" src={readmeGenerator} alt="readme-generator" style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">README Generator</h5>
@@ -64,26 +95,6 @@ function PortfolioCard() {
                         <a className="project-links" href ="https://github.com/awebb8/employee-summary-template" target="_blank">View Github</a>
                         <b> • </b>
                         <a className="project-links" href="https://drive.google.com/file/d/1WtjagtKG1U_6Y4AxiUDYYHU_wp3-fMlW/view" target="_blank">View Video</a>
-                      </div>
-                    </div>
-
-                    <div className="card project-card" style={{width:"26rem"}}>
-                      <img className="crop project-image" src={noteTaker} alt="note-taker" style={{width:"350px", height:"350px"}}/>
-                      <div className="card-body" style={{textAlign:"center"}}>
-                        <h5 className="card-title">Note Taker</h5>
-                        <a className="project-links" href="https://github.com/awebb8/note-taker" target="_blank">View Github</a>
-                        <b> • </b>
-                        <a className="project-links" href="https://obscure-island-16380.herokuapp.com/" target="_blank">View Application</a>
-                      </div>
-                    </div>
-
-                    <div className="card project-card" style={{width:"26rem"}}>
-                      <img className="crop project-image" src={eatDaBurger} alt="eat-da-burger" style={{width:"350px", height:"350px"}}/>
-                      <div className="card-body" style={{textAlign:"center"}}>
-                        <h5 className="card-title">Eat Da Burger</h5>
-                        <a className="project-links" href="https://github.com/awebb8/eat-da-burger" target="_blank">View Github</a>
-                        <b> • </b>
-                        <a className="project-links" href="https://eat-da-burger-alix.herokuapp.com/" target="_blank">View Application</a>
                       </div>
                     </div>
 
