0dbabcf9aabdf1942f23c78b32fcc97b8a1c0c73
Alkawe <alixkwebb@gmail.com>
2021-01-12 15:27:31 -0500

    modified github links

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -26,7 +26,7 @@ function PortfolioCard() {
                       <img className="crop project-image" src={AllHues} alt="AllHues" style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">AllHues</h5>
-                        <a className="project-links" href ="https://github.com/awebb8/allhues" target="_blank">View Github</a>
+                        <a className="project-links" href ="https://github.com/awebb8/allhues" target="_blank">View GitHub</a>
                         <b> • </b>
                         <a className="project-links" href="https://allhues.herokuapp.com/" target="_blank">View Application</a>
                         <b> • </b>
@@ -38,7 +38,7 @@ function PortfolioCard() {
                       <img className="crop project-image" src={whatsForDinner} alt="project-1" style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">What's for Dinner</h5>
-                        <a className="project-links" href ="https://github.com/awebb8/project-1" target="_blank">View Github</a>
+                        <a className="project-links" href ="https://github.com/awebb8/project-1" target="_blank">View GitHub</a>
                         <b> • </b>
                         <a className="project-links" href="https://awebb8.github.io/project-1/" target="_blank">View Application</a>
                         <b> • </b>
@@ -50,7 +50,7 @@ function PortfolioCard() {
                       <img className="crop project-image" src={eatDaBurger} alt="eat-da-burger" style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">Eat Da Burger</h5>
-                        <a className="project-links" href="https://github.com/awebb8/eat-da-burger" target="_blank">View Github</a>
+                        <a className="project-links" href="https://github.com/awebb8/eat-da-burger" target="_blank">View GitHub</a>
                         <b> • </b>
                         <a className="project-links" href="https://eat-da-burger-alix.herokuapp.com/" target="_blank">View Application</a>
                       </div>
@@ -60,7 +60,7 @@ function PortfolioCard() {
                       <img className="crop project-image" src={noteTaker} alt="note-taker" style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">Note Taker</h5>
-                        <a className="project-links" href="https://github.com/awebb8/note-taker" target="_blank">View Github</a>
+                        <a className="project-links" href="https://github.com/awebb8/note-taker" target="_blank">View GitHub</a>
                         <b> • </b>
                         <a className="project-links" href="https://obscure-island-16380.herokuapp.com/" target="_blank">View Application</a>
                       </div>
@@ -70,7 +70,7 @@ function PortfolioCard() {
                       <img className="crop project-image" src={javascriptQuiz} alt="javascript-quiz"  style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">JavaScript Coding Quiz</h5>
-                        <a className="project-links" href ="https://github.com/awebb8/javascript-fundamentals-quiz" target="_blank">View Github</a>
+                        <a className="project-links" href ="https://github.com/awebb8/javascript-fundamentals-quiz" target="_blank">View GitHub</a>
                         <b> • </b>
                         <a className="project-links" href="https://awebb8.github.io/javascript-fundamentals-quiz/" target="_blank">View Application</a>
                       </div>
@@ -80,7 +80,7 @@ function PortfolioCard() {
                       <img className="crop project-image" src={weatherDashboard} alt="weather-dashboard" style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">Weather Dashboard</h5>
-                        <a className="project-links" href ="https://github.com/awebb8/weather-dashboard" target="_blank">View Github</a>
+                        <a className="project-links" href ="https://github.com/awebb8/weather-dashboard" target="_blank">View GitHub</a>
                         <b> • </b>
                         <a className="project-links" href="https://awebb8.github.io/weather-dashboard/" target="_blank">View Application</a>
                       </div>
@@ -90,7 +90,7 @@ function PortfolioCard() {
                       <img className="crop project-image" src={readmeGenerator} alt="readme-generator" style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">README Generator</h5>
-                        <a className="project-links" href="https://github.com/awebb8/readme-generator" target="_blank">View Github</a>
+                        <a className="project-links" href="https://github.com/awebb8/readme-generator" target="_blank">View GitHub</a>
                       </div>
                     </div>
 
@@ -98,7 +98,7 @@ function PortfolioCard() {
                       <img className="crop project-image" src={employeeSummary} alt="employee-summary" style={{width:"350px", height:"350px"}}/>
                       <div className="card-body" style={{textAlign:"center"}}>
                         <h5 className="card-title">Employee Management System</h5>
-                        <a className="project-links" href ="https://github.com/awebb8/employee-summary-template" target="_blank">View Github</a>
+                        <a className="project-links" href ="https://github.com/awebb8/employee-summary-template" target="_blank">View GitHub</a>
                         <b> • </b>
                         <a className="project-links" href="https://drive.google.com/file/d/1WtjagtKG1U_6Y4AxiUDYYHU_wp3-fMlW/view" target="_blank">View Video</a>
                       </div>
