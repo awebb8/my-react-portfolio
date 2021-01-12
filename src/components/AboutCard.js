442475319f3cdf72f584e14831bcb5b33ccfcc09
Alkawe <alixkwebb@gmail.com>
2021-01-11 23:23:14 -0500

    modified About Me section and added separate section for contact information

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -7,7 +7,8 @@ function AboutCard() {
     return (
         <section className="container-fluid" style={{margin:"3%"}}>
             <div className="row">
-                <div style={{width:"94%"}}>
+                <div className="col-sm-7">
+                {/* <div style={{width:"94%"}}> */}
                     <div className="card main-card">
                         <div className="card-header">
                         <h1>About Me</h1>
@@ -15,9 +16,18 @@ function AboutCard() {
                         <div className="card-body">
                             <p className="card-text">
                                 <img src={profilePicture} alt="Alix Webb" style={{width:"200px", height:"200px"}}/>
-                                I'm Alix Webb, and I'm a Web Developer.  I was raised in many places throughout the Eastern U.S., but I currently live in Greenville, SC.  In 2014, I graduated from Virginia Tech with a Bachelor of Science in Industrial and Systems Engineering.  Post-graduation, I used my engineering skills to work for an automotive supplier for six years.  Because I wasn't feeling fulfilled, I sought out a new career direction in hopes of finding my dream job.  I hope to become a Full Stack Web Developer, but more than that, I hope to work for a company that has a great purpose.  My dream company does something for the greater good.  It is a company that is full of people who work hard but also know how to have fun.  It is a company that deeply cares for the well-being of its associates and pushes them to continuously learn and grow. 
-                                If you'd like to learn more about my professional experience, please visit any of the following links:
+                                I’m a full stack web developer with a B.S. in Industrial and Systems Engineering and a minor in Green Engineering from Virginia Tech as well as a certificate in Full Stack Web Development from Georgia Tech.  Currently, I’m living in Greenville, SC, but I’m open to relocating.  I have over 5 years of experience in project management, and I have previously led a few web process improvement projects.  I have experience using HTML, CSS, JavaScript, JQuery, AJAX, JSON,  Node.js, Express.js, React, MERN Stack, MongoDB, MySQL, Heroku, and Amazon Web Services (AWS).  My experience includes working in the requirements, design, coding and implementation, and test phases of development.  Professionally, I enjoy working on a team of highly motivated people that strive for continuous self-improvement and growth.  I prefer a positive and relaxed work environment with open and honest communication.  While working with others, I have found that support, respect, and a willingness to listen to and adopt new ideas are key.  If you’re looking for a new team member who is genuinely committed and takes ownership, please connect with me. 
                             </p>
+                        </div>
+                    </div>
+                {/* </div> */}
+                </div>
+                <div className="col-sm-4">
+                <div className="card main-card">
+                        <div className="card-header">
+                        <h1>Let's Connect</h1>
+                        </div>
+                        <div className="card-body">
                             <li><a href="https://www.linkedin.com/in/alixwebb/">LinkedIn</a></li>
                             <li><a href="https://github.com/awebb8">GitHub</a></li>
                             <li><a href={Resumé}>Resumé</a></li>
